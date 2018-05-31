411cb5627eeda2e16986351eb2d6d2c628b33615
Armend Murtishi <Armend.Murtishi@student.region16ct.org>
2018-05-31 17:48:42 +0000

    Wrote a grammar for the reader.

--- src/Reader.java
+++ src/Reader.java
@@ -4,7 +4,16 @@ import java.util.ArrayList;
 
 public class Reader
 {
-    // Parse the file given, generating an ArrayList of students and checking for any errors.
+    /* Parse the file given, generating an ArrayList of students and checking for any errors.
+     * The best way to do this is to write a grammar that we can interpret in this function.
+     * Grammar:
+     * List_Students
+     *   List \n Student
+     * Student
+     *   Name (String), Grade (int) \n "REQUIRED:" \n List_Classes \n "REQUESTED:" \n List_Classes \n
+     * List_Classes
+     *    List_Classes \n Class (String)
+     */
     public static ArrayList<Student> read(String file)
     {
         ArrayList<Student> result = new ArrayList<Student>();
