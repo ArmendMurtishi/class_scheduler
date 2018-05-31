3d6de98c0d4a668b9513d37d5343f0244d39e784
Eric W <eric.wrogg@student.region16ct.org>
2018-05-31 18:01:05 +0000

    Started main method.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.io.*;
 public class Main
 {
     // Get the file from args, use Scheduler to generate the schedules, and save them to unique files.
     public static void main(String[] args)
     {
+        
     }
+    /**
+     * This method inputs the student objects state into the method, and then 
+     */
+    public void whenWriteStringUsingBufferedWritter_thenCorrect(String fileName) throws IOException 
+        {
+            String name = "Hello";
+            String grade = 
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(str);
+     
+            writer.close();
+        }
 }
