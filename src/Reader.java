dcf6618b629adc272310d1b285fb80b4df1c6d94
Armend Murtishi <Armend.Murtishi@student.region16ct.org>
2018-06-04 17:19:54 +0000

    Continued work on the grammar.

--- src/Reader.java
+++ src/Reader.java
@@ -8,17 +8,29 @@ public class Reader
      * The best way to do this is to write a grammar that we can interpret in this function.
      * Grammar:
      * List_Students
-     *   List \n Student
+     *   List_Students \n Student
      * Student
      *   Name (String), Grade (int) \n "REQUIRED:" \n List_Classes \n "REQUESTED:" \n List_Classes \n
      * List_Classes
      *    List_Classes \n Class (String)
      */
-    public static ArrayList<Student> read(String file) { return list_students(file); }
+    public static ArrayList<Student> read(String file) { return list_students(new Scanner(file)); }
     
-    private static ArrayList<Student> list_students(String file)
+    private static Student student(Scanner file)
+    {
+        while(file.hasNextLine())
+        {
+            // Match a comma and then 0 or more spaces
+            file.useDelimiter(",\\s*");
+        }
+    }
+    private static ArrayList<Student> list_students(Scanner file)
     {
         ArrayList<Student> res = new ArrayList<Student>();
+        while((Student s = student(file)) != null)
+        {
+            res.add(student);
+        }
         return res;
     }
 }
