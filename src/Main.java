412e96bbb2b82050fec2055e9d9a206e6523fd74
Eric W <eric.wrogg@student.region16ct.org>
2018-06-06 17:57:05 +0000

    June 6th commit of main class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import java.io.*;
+import java.util.ArrayList;
 public class Main
 {
     // Get the file from args, use Scheduler to generate the schedules, and save them to unique files.
     public static void main(String[] args)
     {
+        File fileOfStudents = File(README);
+        ArrayList<Student> students = read(fileOfStudents);
+        File globalSchedule = getGlobalSchedule();
+        File studentSchedule = getStudentSchedule();
         
     }
     /**
@@ -11,6 +16,7 @@ public class Main
      * Then writes the students into the file that will be read to the schedule 
      * 
      * 6 errors
+     * 5 of them are on the name of the method
      */
     public void writeFile(getStudentSchedules()) throws IOException 
     {
